2242aeff7f2f527e55f7c7adb4c1eae31c9b9880
Michelle Cone <tesmichelle3@gmail.com>
2024-04-04 16:03:22 +0300

    rho_values changed

--- compute_density.py
+++ compute_density.py
@@ -4,7 +4,9 @@ from tqdm import tqdm
 
 from interface import Matrix, state, lineage, densityTaTb, stateToNum, Mdim
 
-rho_values = np.linspace(0.001, 20, 40)
+rho_values = np.array([0.001, 0.2, 0.4, 0.6, 0.8, 1.2, 2.6, 2.8, 3.2, 3.4,
+                     3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.5, 6., 6.5, 7.,
+                     7.5, 8., 8.5, 9., 9.5, 15., 20.], float)
 # 0, 1, 2, 3 => rho_i = 0, pop_a = 0, 1, 0, 1, pop_b = 0, 0, 1, 1
 rho_i = int(sys.argv[1]) // 4
 pop_a = int(sys.argv[1]) % 2
